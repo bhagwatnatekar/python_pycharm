ecbad89d5230b3eb9987385db24a3a41086f025b
Bhagwat Natekar <bhagwatnatekar96@gmail.com>
2025-06-25 20:01:31 +0530

    Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,8 @@
+#welcome line
+print("Welcome to the Band Name Generator.")
+#taking input city name
+city = input("What is the name of the city you grew up in?\n")
+#taking input petname
+pet_name = input("What's your pet name?\n")
+#String concatenation
+print("Your band name could be:", city + pet_name)
