4e83d579de14dea5584aae2f5d5afcccb8f1cb53
Bhagwat Natekar <bhagwatnatekar96@gmail.com>
2025-07-03 11:07:00 +0530

    Square root code

---
+++ Python_practice/square_root.py
@@ -0,0 +1,10 @@
+# square_root = 0
+number = int(input("Enter the number to find square root: "))
+square_root = number ** (1/2)
+print(square_root)
+
+# Using math import
+import math
+number = int(input("Enter the number to find square root: "))
+square_root = math.sqrt(number)
+print(square_root)
